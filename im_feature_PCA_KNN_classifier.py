77324786f663634fb3659baddf388001f82b6fdb
unknown <l.sanchezca.2022@alumnos.urjc.es>
2026-05-04 13:12:13 +0200

    newfile

---
+++ im_feature_PCA_KNN_classifier.py
@@ -0,0 +1,12 @@
+"""PCA and KNN Classifier for Image Features
+Ejercicio 2: Probar otras alteranativas para el reconocimiento"""
+
+
+import cv2
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from ocr_classifier import OCRClassifier
+
+class PcaKnnClassifier(OCRClassifier):
+    pass
